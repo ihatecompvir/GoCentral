7008d5fd5db7259617a9db67ed24ac22fa790028
ihatecompvir <ihatecompvir@live.com>
2023-02-02 09:45:19 -0800

    Implement NAT traversal probes

--- go.mod
+++ go.mod
@@ -4,15 +4,19 @@ go 1.16
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/ihatecompvir/nex-go v0.0.0-20221216013912-1ef427fb8deb
-	github.com/ihatecompvir/nex-protocols-go v0.0.0-20221217120721-8d22b6a68ebe
+	github.com/ihatecompvir/nex-go v0.0.0-20230202172142-2997951cca7a
+	github.com/ihatecompvir/nex-protocols-go v0.0.0-20230202172446-03241ef34b85
 	github.com/jinzhu/copier v0.3.5
-	github.com/klauspost/compress v1.15.13 // indirect
-	github.com/montanaflynn/stats v0.6.6 // indirect
-	github.com/superwhiskers/crunch/v3 v3.5.6 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/montanaflynn/stats v0.7.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/superwhiskers/crunch/v3 v3.5.7 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	go.mongodb.org/mongo-driver v1.11.1
-	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
 )
--- server.go
+++ server.go
@@ -13,6 +13,7 @@ import (
 	"rb3server/models"
 	"rb3server/protocols/jsonproto"
 	"regexp"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -403,6 +404,9 @@ func mainSecure(database *mongo.Database) {
 					{"$set", bson.D{{"int_station_url", internalStationURL}}},
 				},
 			)
+
+			client.SetExternalStationURL(stationURL)
+			client.SetConnectionID(uint32(randomRVCID))
 
 			if err != nil {
 				log.Fatalln(err)
@@ -986,17 +990,15 @@ func mainSecure(database *mongo.Database) {
 
 	})
 
-	natTraversalServer.InitiateProbe(func(err error, client *nex.Client, callID uint32, stationURL string) {
-		log.Printf("Client is performing a NAT traversal probe to %s...\n", stationURL)
+	natTraversalServer.RequestProbeInitiation(func(err error, client *nex.Client, callID uint32, stationURLs []string) {
+		log.Printf("Client wants to perform NAT traversal probes to %v servers...\n", len(stationURLs))
 
 		rmcResponseStream := nex.NewStream()
 
 		rmcResponseBody := rmcResponseStream.Bytes()
 
 		rmcResponse := nex.NewRMCResponse(nexproto.NATTraversalID, callID)
-		rmcResponse.SetSuccess(nexproto.InitiateProbe, rmcResponseBody)
-
-		rmcResponseBytes := rmcResponse.Bytes()
+		rmcResponse.SetSuccess(nexproto.RequestProbeInitiation, rmcResponseBody)
 
 		responsePacket, _ := nex.NewPacketV0(client, nil)
 
@@ -1005,13 +1007,47 @@ func mainSecure(database *mongo.Database) {
 		responsePacket.SetDestination(0x3F)
 		responsePacket.SetType(nex.DataPacket)
 
-		newArray := make([]byte, len(rmcResponseBytes)+1)
-		copy(newArray[1:len(rmcResponseBytes)+1], rmcResponseBytes)
-		responsePacket.SetPayload(newArray)
-
 		responsePacket.AddFlag(nex.FlagNeedsAck)
 
 		nexServer.Send(responsePacket)
+
+		rmcMessage := nex.RMCRequest{}
+		rmcMessage.SetProtocolID(nexproto.NATTraversalID)
+		rmcMessage.SetCallID(0xffff0000 + callID)
+		rmcMessage.SetMethodID(nexproto.InitiateProbe)
+		rmcRequestStream := nex.NewStreamOut(nexServer)
+		rmcRequestStream.WriteString(client.ExternalStationURL())
+		rmcRequestBody := rmcRequestStream.Bytes()
+		rmcMessage.SetParameters(rmcRequestBody)
+		rmcMessageBytes := rmcMessage.Bytes()
+
+		// loop through every station URL in the probe request and send InitiateProbe to them
+		// This should make all targets respond to NAT probes from the joining client
+		for _, target := range stationURLs {
+			targetUrl := nex.NewStationURL(target)
+			log.Println("Sending NAT probe to " + target)
+			targetRvcID, _ := strconv.Atoi(targetUrl.RVCID())
+			targetClient := nexServer.FindClientFromConnectionID(uint32(targetRvcID))
+			if targetClient != nil {
+				var messagePacket nex.PacketInterface
+
+				messagePacket, _ = nex.NewPacketV0(targetClient, nil)
+				messagePacket.SetVersion(0)
+
+				messagePacket.SetSource(0x31)
+				messagePacket.SetDestination(0x3F)
+				messagePacket.SetType(nex.DataPacket)
+				messagePacket.SetPayload(rmcMessageBytes)
+
+				messagePacket.AddFlag(nex.FlagNeedsAck)
+				messagePacket.AddFlag(nex.FlagReliable)
+
+				nexServer.Send(messagePacket)
+			} else {
+				log.Printf("Could not find active client with RVCID %v\n", targetRvcID)
+			}
+		}
+
 	})
 
 	accountManagementServer.NintendoCreateAccount(func(err error, client *nex.Client, callID uint32, username string, key string, groups uint32, email string) {
