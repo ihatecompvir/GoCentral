6b8ec328d9397c464dc2a8508aebcc2358ac0bf3
ihatecompvir <ihatecompvir@live.com>
2024-08-03 03:38:41 -0700

    Binary data getter improvements

--- servers/getbinarydata.go
+++ servers/getbinarydata.go
@@ -2,12 +2,31 @@ package servers
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"path/filepath"
+	"rb3server/database"
+	"rb3server/models"
 	"rb3server/quazal"
+	"strings"
 
 	"github.com/ihatecompvir/nex-go"
 	nexproto "github.com/ihatecompvir/nex-protocols-go"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+func SanitizePath(path string) string {
+	// check for any invalid characters in the path
+	// if any are found, replace them with an underscore
+	invalidChars := []string{"\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\\r", "\\n", "\x0a", "\x0d", ".", "\x00"}
+
+	for _, char := range invalidChars {
+		path = strings.ReplaceAll(path, char, "_")
+	}
+
+	return path
+}
 
 func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string) {
 	// metadata looks like this
@@ -35,10 +54,28 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 		return
 	}
 
-	// switch on the type
+	var filePath string
+
+	var platformExtension string
+
+	// we don't want to try to send a png_ps3 to xbox and etc.
+	// so we need to check the platform and set the extension accordingly
+	switch client.Platform() {
+	case 0:
+		platformExtension = "png_xbox"
+	case 1:
+	case 3:
+		platformExtension = "png_ps3"
+	case 2:
+		platformExtension = "png_wii"
+	default:
+		log.Printf("Unsupported platform %d in requested metadata", client.Platform())
+		SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+		return
+	}
+
 	switch dataType {
 	case "setlist_art":
-		// get the setlist guid
 		setlistGUID, ok := metadataMap["setlist_guid"].(string)
 
 		if !ok {
@@ -47,7 +84,18 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 			return
 		}
 
-		// get the revision
+		// make sure the setlist with the specified GUID actually exists
+		setlistsCollection := database.GocentralDatabase.Collection("setlists")
+
+		var setlist models.Setlist
+		err = setlistsCollection.FindOne(nil, bson.M{"guid": setlistGUID}).Decode(&setlist)
+
+		if err != nil {
+			log.Println("Error finding setlist with GUID ", setlistGUID)
+			SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+			return
+		}
+
 		revisionFloat, ok := metadataMap["revision"].(float64)
 
 		if !ok {
@@ -56,13 +104,19 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 			return
 		}
 
-		// convert float64 to int64
 		revision := int64(revisionFloat)
 
-		log.Println("Getting setlist art for setlist %s with revision %d", setlistGUID, revision)
+		filePath = fmt.Sprintf("binary_data/setlist_art/%s/%d."+platformExtension, setlistGUID, revision)
+
+		filePath = filepath.Clean(SanitizePath(filePath))
+
+		if !strings.HasPrefix(filePath, "binary_data") {
+			log.Println("Invalid path: ", filePath)
+			SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+			return
+		}
 
 	case "battle_art":
-		// get the revision
 		revisionFloat, ok := metadataMap["revision"].(float64)
 
 		if !ok {
@@ -71,16 +125,34 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 			return
 		}
 
-		// convert float64 to int64
 		revision := int64(revisionFloat)
 
 		// battle_art can optionally have battle_id; try to get it, but dont fail if it cant be found
 		battleID, _ := metadataMap["battle_id"].(float64)
 
-		log.Println("Getting battle art with revision %d with battle ID %d", revision, int64(battleID))
+		// make sure the setlist with the specified battle ID actually exists
+		setlistsCollection := database.GocentralDatabase.Collection("setlists")
+
+		var setlist models.Setlist
+		err = setlistsCollection.FindOne(nil, bson.M{"setlist_id": battleID}).Decode(&setlist)
+
+		if err != nil {
+			log.Println("Error finding battle with battle ID ", battleID)
+			SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+			return
+		}
+
+		filePath = fmt.Sprintf("binary_data/battle_art/%d/%d."+platformExtension, int64(battleID), revision)
+
+		filePath = filepath.Clean(SanitizePath(filePath))
+
+		if !strings.HasPrefix(filePath, "binary_data") {
+			log.Println("Invalid path: ", filePath)
+			SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+			return
+		}
 
 	case "band_logo":
-		// get the band id
 		bandIDFloat, ok := metadataMap["band_id"].(float64)
 
 		if !ok {
@@ -89,10 +161,8 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 			return
 		}
 
-		// convert float64 to int64
 		bandID := int64(bandIDFloat)
 
-		// get the revision
 		revisionFloat, ok := metadataMap["revision"].(float64)
 
 		if !ok {
@@ -101,10 +171,17 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 			return
 		}
 
-		// convert float64 to int64
 		revision := int64(revisionFloat)
 
-		log.Println("Getting band logo for band %d with revision %d", bandID, revision)
+		filePath = fmt.Sprintf("binary_data/band_logo/%d/%d."+platformExtension, bandID, revision)
+
+		filePath = filepath.Clean(SanitizePath(filePath))
+
+		if !strings.HasPrefix(filePath, "binary_data") {
+			log.Println("Invalid path: ", filePath)
+			SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
+			return
+		}
 
 	default:
 		log.Println("Unsupported type %s in requested metadata", dataType)
@@ -112,17 +189,17 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 		return
 	}
 
-	rmcResponseStream := nex.NewStream()
-
+	// Read the file from disk
+	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		log.Println("Error reading test.png_ps3: ", err)
+		log.Println("Error reading file from disk: ", err)
 		SendErrorCode(SecureServer, client, nexproto.RBBinaryDataProtocolID, callID, quazal.UnknownError)
 		return
 	}
 
-	// TODO: implement loading binary data from disk
-	rmcResponseStream.WriteBuffer([]byte{0x00})
-	rmcResponseStream.WriteBufferString("{}") // the game doesn't really care what we send here so just send empty json
+	rmcResponseStream := nex.NewStream()
+	rmcResponseStream.WriteBuffer(data)
+	rmcResponseStream.WriteBufferString("{}")
 
 	rmcResponseBody := rmcResponseStream.Bytes()
 
@@ -144,5 +221,4 @@ func GetBinaryData(err error, client *nex.Client, callID uint32, metadata string
 	responsePacket.AddFlag(nex.FlagReliable)
 
 	SecureServer.Send(responsePacket)
-
 }
